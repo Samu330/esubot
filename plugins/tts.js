57808b39e12fb501f02e83fea5fca2bcb31c7928
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-01-28 14:57:17 -0600

    Update tts.js

--- plugins/tts.js
+++ plugins/tts.js
@@ -3,10 +3,10 @@ let fs = require('fs')
 let path = require('path')
 let { spawn } = require('child_process')
 let handler = async (m, { conn, args }) => {
-  if (args.length < 2) return m.reply('Tidak ada teks nya')
+  if (args.length < 2) return m.reply('No hay texto')
   conn.sendFile(m.chat, await tts(args[0], args.slice(1).join(' ')).catch(err => m.reply(err + '')), 'tts.opus', null, m, true)
 }
-handler.help = ['tts <lang> <teks>']
+handler.help = ['tts <idioma> <texto>']
 handler.tags = ['tools']
 handler.command = /^g?tts$/i
 module.exports = handler
