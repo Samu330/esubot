3ee761ca31edda5b6ce56398756195eddff2a54a
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-01-28 14:49:43 -0600

    Update speed.js

--- plugins/speed.js
+++ plugins/speed.js
@@ -1,7 +1,7 @@
 let { performance } = require('perf_hooks')
 let handler = async (m, { conn }) => {
   let old = performance.now()
-  await m.reply('_Testing speed..._')
+  await m.reply('_Velocidad de prueba..._')
   let neww = performance.now()
   m.reply(neww - old + 'ms')
 }
