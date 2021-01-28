799338070b34509782d7db3fed660314a1968069
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-01-28 14:56:15 -0600

    Update ttp.js

--- plugins/ttp.js
+++ plugins/ttp.js
@@ -16,7 +16,7 @@ let handler  = async (m, { conn, text }) => {
     })
   }
 }
-handler.help = ['ttp <teks>']
+handler.help = ['ttp <texto>']
 handler.tags = ['sticker']
 handler.command = /^ttp$/i
 handler.owner = false
