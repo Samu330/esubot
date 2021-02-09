4e8817aacaee5f69d1119e3ced9e99ffa0b7e4c8
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-02-08 18:14:22 -0600

    Update nulis.js

--- plugins/nulis.js
+++ plugins/nulis.js
@@ -51,7 +51,7 @@ let handler  = async (m, { conn, args }) => {
   ])
   .on('error', e => conn.reply(m.chat, util.format(e), m))
   .on('exit', () => {
-    conn.sendFile(m.chat, outputPath, 'nulis.jpg', 'Hati² ketahuan:v', m)
+    conn.sendFile(m.chat, outputPath, 'nulis.jpg', '_READY²_ lista tu tarea:v', m)
   })
 }
 handler.help = ['n'].map(v => v + 'ulis <teks>')
