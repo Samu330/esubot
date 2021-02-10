43f3765603540a7a39fcd5e98f3dc3bf67e40fd7
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-02-10 11:07:47 -0600

    Delete plugins directory

--- plugins/creator.js
+++
@@ -1,9 +0,0 @@
-let handler = function (m) {
-  this.sendContact(m.chat, '529984907794', 'Samu330', m)
-}
-handler.help = ['owner', 'creator']
-handler.tags = ['info']
-
-handler.command = /^(owner|creator)$/i
-
-module.exports = handler
