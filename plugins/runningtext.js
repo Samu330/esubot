7016bfa1839cfaa9f85aea5515271e60d0c567cb
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-02-10 09:48:12 -0600

    Update runningtext.js

--- plugins/runningtext.js
+++ plugins/runningtext.js
@@ -2,8 +2,8 @@ let handler = async (m, { conn, text }) => {
   let q
   try { q = m.quoted.download() }
   catch (e) { q = m.download() }
-  m.reply('_Sedang membuat..._\n*Mohon tunggu sekitar 1 menit*')
-  running(await q).then(vid => conn.sendFile(m.chat, vid, 'run.mp4', '*© Nurutomo*\nMade with FFmpeg', m))
+  m.reply('_En proceso..._\n*Espere aproximadamente 1 minuto*')
+  running(await q).then(vid => conn.sendFile(m.chat, vid, 'run.mp4', '*© Samu330*\nMade with FFmpeg', m))
 }
 handler.help = ['run']
 handler.tags = ['tools']
