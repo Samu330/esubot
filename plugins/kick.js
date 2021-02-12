86681d3cabc2f46151e79df8a9f08b4e88905ab0
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-02-12 08:58:20 -0600

    Update kick.js

--- plugins/kick.js
+++ plugins/kick.js
@@ -1,11 +1,11 @@
 let handler = async (m, { conn, args }) => {
-  let ownerGroup = m.chat.split`-`[0] + '@s.whatsapp.net'
-  let users = m.mentionedJid.filter(u => !(u == ownerGroup || u.includes(conn.user.jid)))
+  let adminGroup = m.chat.split`-`[0] + '@s.whatsapp.net'
+  let users = m.mentionedJid.filter(u => !(u == adminGroup || u.includes(conn.user.jid)))
   for (let user of users) if (user.endsWith('@s.whatsapp.net')) await conn.groupRemove(m.chat, [user])
 }
-handler.help = ['kick','-'].map(v => 'kick' + v + ' @user')
+handler.help = ['kick'].map(v => 'kick' + v + ' @user')
 handler.tags = ['admin']
-handler.command = /^(kick|o\-)$/i
+handler.command = /^kick$/i
 handler.owner = true
 handler.mods = false
 handler.premium = false
