0a8d11ff531d71279e1b998693ea91e85787e6c6
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-02-12 08:50:21 -0600

    Update add.js

--- plugins/add.js
+++ plugins/add.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, text }) => {
 }
 handler.help = ['add', '+'].map(v => 'o' + v + ' @user')
 handler.tags = ['admin']
-handler.command = /^(oadd|o\+)$/i
+handler.command = /^(add|o\+)$/i
 handler.owner = true
 handler.mods = false
 handler.premium = false
