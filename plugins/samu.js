b29aa276df3f97e63ab3bd0c9669e1732ff6f9fa
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-02-15 18:59:20 -0500

    Update samu.js

--- plugins/samu.js
+++ plugins/samu.js
@@ -2,6 +2,11 @@ let handler = async m => m.reply(`
   ^    ^    ^    ^    ^  
  /S\  /m\  /3\  /3\  /0\ 
 <___><___><___><___><___>
+
+COMO CREAR ESTE BOT:
+
+https://youtu.be/chMc57gjmkI
+
 𝙈𝙮 𝘾𝙝𝙖𝙣𝙚𝙡:
 
 https://www.youtube.com/channel/UCHD4T8Pfcv5PFVzsAbfAPZA
