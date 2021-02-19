73b03057d8e775440384697d2dac651a4a5789c8
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-02-19 07:08:50 -0600

    Update promote.js

--- plugins/promote.js
+++ plugins/promote.js
@@ -2,9 +2,7 @@ let handler = async (m, { conn, args }) => {
   let users = m.mentionedJid
   conn.groupMakeAdmin(m.chat, users)
 }
-handler.help = ['promote','admin','^', '↑'].map(v => v + ' @user')
-handler.tags = ['admin']
-handler.command = /^(promote|admin|\^|↑)$/i
+handler.command = /^(promote|admin|\^)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
@@ -17,4 +15,3 @@ handler.botAdmin = true
 handler.fail = null
 
 module.exports = handler
-
