c68b8d1cf7a2bde522388e18386bbcdcaaf231b1
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-02-19 07:09:29 -0600

    Update demote.js

--- plugins/demote.js
+++ plugins/demote.js
@@ -1,10 +1,8 @@
-let handler = async (m, { conn, args, usedPrefix }) => {
+let handler = async (m, { conn, args }) => {
   let users = m.mentionedJid
-  for (let user of users) conn.groupDemoteAdmin(m.chat, user).catch(console.log)
+  conn.groupDemoteAdmin(m.chat, users)
 }
-handler.help = ['demote','member','↓'].map(v => v + ' @user')
-handler.tags = ['admin']
-handler.command = /^(demote|member|↓)$/i
+handler.command = /^(demote|member|v)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
