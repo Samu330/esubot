59dd1278959a8c86e5e6ff3062bf30d422b4973e
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-03-01 12:42:50 -0600

    Update unregister.js

--- plugins/unregister.js
+++ plugins/unregister.js
@@ -1,1 +1,16 @@
+const { createHash } = require('crypto')
+let handler = async function (m, { args }) {
+  if (!args[0]) throw 'El número de serie está vacío'
+  let user = global.DATABASE._data.users[m.sender]
+  let sn = createHash('md5').update(m.sender).digest('hex')
+  if (args[0] !== sn) throw 'Serial Number invalido'
+  user.registered = false
+  m.reply(`Registro eliminado!`)
+}
+handler.help = ['', 'ister'].map(v => 'unreg' + v + ' <SN|SERIAL NUMBER>')
+handler.tags = ['exp']
 
+handler.command = /^unreg(ister)?$/i
+handler.register = true
+
+module.exports = handler
