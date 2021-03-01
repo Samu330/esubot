f10ef0cdef6e46fe3fd22702d684cb3b767f715e
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-03-01 13:13:33 -0600

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,5 +1,5 @@
 let handler = function (m) {
-  this.sendContact(m.chat, '529984907794', 'Samu330', m)
+  this.sendContact(m.chat, '+52 998 490 7794', '👑Samu330👑', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
