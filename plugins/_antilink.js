6c3f4628350ba11b92ee72046ab7014a8a37886b
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-03-05 13:42:39 -0600

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -8,9 +8,7 @@ handler.before = m => {
 
   if (chat.antiLink && isGroupLink) m.reply('Eliminando participante!!')
   
-  let ownerGroup = m.chat.split`-`[0] + '@s.whatsapp.net'
-  let users = m.mentionedJid.filter(u => !(u == ownerGroup || u.includes(conn.user.jid)))
-  for (let user of users) if (user.endsWith('@s.whatsapp.net')) await conn.groupRemove(m.chat, [user])
+  await conn.groupRemove(m.chat, [user])
 
   return true
 }
