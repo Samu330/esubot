286168a54e88b8e85293fbf8f3f3a707ea522f58
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-03-06 12:21:15 -0600

    Update demote.js

--- plugins/demote.js
+++ plugins/demote.js
@@ -1,20 +1,26 @@
-let handler = async (m, { conn, args }) => {
-  let users = m.mentionedJid
-  conn.groupDemoteAdmin(m.chat, users)
+let handler = async (m, { conn, text }) => {
+	if(isNaN(text) && !text.match(/@/g)){
+		return conn.reply(m.chat, `*Uso apropiado*\n\n.demote @user\n.demote -> reply chat`, m)
+	}else if(isNaN(text)) {
+		var number = text.split`@`[1]
+	}else if(!isNaN(text)) {
+		var number = text
+	}
+	
+	if(!text && !m.quoted) return conn.reply(m.chat, `*Uso apropiado*\n\n.demote @user\n.demote -> reply chat`, m)
+	if(number.length > 15 || (number.length < 9 && number.length > 0)) return conn.reply(m.chat, `*Ingrese el número correcto !*`, m)
+	
+try {
+	if(text) {
+		var user = number + '@s.whatsapp.net'
+	} else if(m.quoted.sender) {
+		var user = m.quoted.sender
+	} else if(m.mentionedJid) {
+		var user = number + '@s.whatsapp.net'
+	} 
+} catch (e) {
+		} finally {
+			conn.groupDemoteAdmin(m.chat, [user]).catch(console.log)
+			conn.reply(m.chat, `*Target has demoted.*`, m)
+	}	
 }
-handler.help = ['demote','member','v'].map(v => '' + v + ' @user')
-handler.command = /^(demote|member|v)$/i
-handler.tags = ['admin']
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = true
-handler.private = false
-
-handler.admin = true
-handler.botAdmin = true
-
-handler.fail = null
-
-module.exports = handler
-
