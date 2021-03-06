7bf1faa399b063cf2cc177a51fb783043c442e61
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-03-06 13:07:11 -0600

    Update ownerhidetag.js

--- plugins/ownerhidetag.js
+++ plugins/ownerhidetag.js
@@ -1,1 +1,21 @@
+let { MessageType } = require('@adiwajshing/baileys')
 
+let handler = async (m, { conn, text }) => {
+  let users = (await conn.groupMetadata(m.chat)).participants.map(u => u.jid)
+  conn.sendMessage(m.chat, text, MessageType.extendedText, { contextInfo: { mentionedJid: users } })
+}
+handler.help = ['ohidetag','otag'].map(v => v + ' *text*')
+handler.tags = ['owner']
+handler.command = /^(ohidetag|otag)$/i
+handler.owner = true
+handler.mods = false
+handler.premium = false
+handler.group = true
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+module.exports = handler
