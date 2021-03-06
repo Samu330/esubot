408450b318e684086472f2f869ccbc41be8ab16c
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-03-06 13:32:41 -0600

    Update simsimi.js

--- plugins/simsimi.js
+++ plugins/simsimi.js
@@ -1,13 +1,28 @@
+let { Presence } = require('@adiwajshing/baileys')
 let fetch = require('node-fetch')
-let handler = async (m, { text }) => {
-  let res = await fetch(global.API('xteam', '/simsimi', { kata: text }, 'APIKEY'))
-  let json = await res.json()
-  if (json.status) m.reply(json.jawaban)
-  else throw json
-}
-handler.help = ['simi', 'simsimi', 'simih'].map(v => v + ' <teks>')
-handler.tags = ['fun']
-handler.command = /^((sim)?simi|simih)$/i
 
+let handler  = async (m, { conn, args, usedPrefix, command }) => {
+	if (!args || !args[0]) return conn.reply(m.chat, `Formato incorrecto!\n\n*Ejemplpo* : _${usedPrefix + command} Hola simi_`, m)
+	let text = args.join` `
+	fetch("https://simsumi.herokuapp.com/api?text=" + encodeURIComponent(text) + "&lang=id")
+  .then(res => res.json())
+  .then(batch => {
+    conn.updatePresence(m.chat, Presence.composing) 
+    conn.reply(m.chat, `${batch.success}`, m)
+  }) .catch(() => { conn.reply(m.chat, `_¡La función simi es errónea! Perdón :(_`, m) })
+}
+handler.help = ['simi','s'].map(v => v + ' *text*')
+handler.tags = ['fun']
+handler.command = /^(simi|s)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+handler.admin = false
+handler.botAdmin = false
+handler.fail = null
+handler.limit = true
+handler.exp = 750
 module.exports = handler
 
