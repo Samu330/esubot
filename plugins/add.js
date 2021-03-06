52ffb41bd25dbaabf4141293a7cf9a83fc9d2e38
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-03-06 14:03:22 -0600

    Update add.js

--- plugins/add.js
+++ plugins/add.js
@@ -1,19 +1,36 @@
 let handler = async (m, { conn, text }) => {
-  let users = text.split`,`.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v.length > 20)
-  await conn.groupAdd(m.chat, users)
+	if(isNaN(text)) {
+		var number = text.split`@`[1]
+	} else if(!isNaN(text)) {
+		var number = text
+	}
+	
+	if(!text && !m.quoted) return conn.reply(m.chat, `*Give a number or reply chat target.*`, m)
+	if(number.length > 15) return conn.reply(m.chat, `*Format is Invalid.*`, m)
+	
+try {
+	if(text) {
+		var user = number + '@s.whatsapp.net'
+	} else if(m.quoted.sender) {
+		var user = m.quoted.sender
+	} else if(m.mentionedJid) {
+		var user = number + '@s.whatsapp.net'
+	} 
+} catch (e) {
+		} finally {
+			await conn.groupAdd(m.chat, [user])
+	}	
 }
-handler.help = ['add', '+'].map(v => '' + v + ' @user')
-handler.command = [/^(add|\+)$/i]
-handler.tags = ['admin']
+handler.help = ['add *52xx*','add *(reply)*']
+handler.tags = ['group admin']
+handler.command = /^(add)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
 handler.group = true
 handler.private = false
-
 handler.admin = true
 handler.botAdmin = true
-
+handler.limit = true
 handler.fail = null
-
 module.exports = handler
