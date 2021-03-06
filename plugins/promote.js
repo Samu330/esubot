e071a95aefad4162103acc5ae630f1bc4f9bcef6
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-03-06 12:29:05 -0600

    Update promote.js

--- plugins/promote.js
+++ plugins/promote.js
@@ -1,19 +1,38 @@
-let handler = async (m, { conn, args }) => {
-  let users = m.mentionedJid
-  conn.groupMakeAdmin(m.chat, users)
+let handler = async (m, { conn, text }) => {
+	if(isNaN(text) && !text.match(/@/g)){
+		return conn.reply(m.chat, `*Uso apropiado*\n\n.promote @user\n.promote -> reply chat`, m)
+	}else if(isNaN(text)) {
+		var number = text.split`@`[1]
+	}else if(!isNaN(text)) {
+		var number = text
+	}
+	
+	if(!text && !m.quoted) return conn.reply(m.chat, `*Uso apropiado*\n\n.promote @user\n.promote -> reply chat`, m)
+	if(number.length > 15 || (number.length < 9 && number.length > 0)) return conn.reply(m.chat, `*Ingrese el número correcto !*`, m)
+	
+try {
+	if(text) {
+		var user = number + '@s.whatsapp.net'
+	} else if(m.quoted.sender) {
+		var user = m.quoted.sender
+	} else if(m.mentionedJid) {
+		var user = number + '@s.whatsapp.net'
+	} 
+} catch (e) {
+		} finally {
+			conn.groupMakeAdmin(m.chat, [user]).catch(console.log)
+			conn.reply(m.chat, `*Target has promoted.*`, m)
+	}	
 }
-handler.help = ['promote','admin','^'].map(v => '' + v + ' @user')
-handler.command = /^(promote|admin|\^)$/i
-handler.tags = ['admin']
+handler.help = ['*62xx*','*@user*','*(reply)*'].map(v => 'promote ' + v)
+handler.tags = ['group admin']
+handler.command = /^(promote)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
 handler.group = true
 handler.private = false
-
 handler.admin = true
 handler.botAdmin = true
-
 handler.fail = null
-
 module.exports = handler
