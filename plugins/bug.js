19fda419a4b187a6c34e0c59ae26d6164acfb900
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-03-06 11:47:59 -0600

    Update bug.js

--- plugins/bug.js
+++ plugins/bug.js
@@ -3,9 +3,10 @@ let handler  = async (m, { conn, text}) => {
   if (!text){
     conn.reply(m.chat,'Ingresa el informe de errores de un amigo :)',m)
   }else {
-    conn.reply(global.owner + '@s.whatsapp.net',`*Laporan Bug daría gas de g]
+    conn.reply(global.owner + '@s.whatsapp.net',`*Reporte enviado de: @${m.sender.split('@')[0]}*\n\n${text}`, m,{contextInfo: {
+      mentionedJid: [m.sender]
     }})
-    conn.reply(m.chat,`*Se envió un informe de error satisfactorio*\n\n¿Sabías el hecho impactante de que los informes falsos pueden ocasionar ? *BANNED*`,m)
+    conn.reply(m.chat,`Se envió un informe de error satisfactorio\n\nSabías el hecho impactante de que los informes falsos pueden ocasionar? *BANNED*`,m)
   }
 }
 handler.help = ['bug *text*']
