aa7ec659a2a3029f74c2c8ee61381ca0001516af
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-03-06 12:02:47 -0500

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -2,10 +2,10 @@ let limit = 30
 let yts = require('yt-search')
 const { servers, yta, ytv } = require('../lib/y2mate')
 let handler = async (m, { conn, command, text, isPrems, isOwner }) => {
-  if (!text) throw 'Cari apa?'
+  if (!text) throw 'aque estas buscando ?'
   let results = await yts(text)
   let vid = results.all.find(video => video.seconds < 3600)
-  if (!vid) throw 'Video/Audio Tidak ditemukan'
+  if (!vid) throw 'No pude encontrar el Video/Audio'
   let { dl_link, thumb, title, filesize, filesizeF} = await (/2$/.test(command) ? ytv : yta)(vid.url, 'id4')
   let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
   conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
