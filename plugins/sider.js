ddad0f76f1c920deb4e32e8d6fe230b947b835f3
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-03-06 12:38:26 -0600

    Update sider.js

--- plugins/sider.js
+++ plugins/sider.js
@@ -23,7 +23,7 @@ let handler = async (m, { conn, text, participants }) => {
 			}
 		}
 	}
-	if(total == 0) return conn.reply(m.chat, `*Este grupo no tiene sider.*`, m) 
+	if(total == 0) return conn.reply(m.chat, `*Este grupo no tiene fantasmas:D.*`, m) 
 	// conn.reply(m.chat, `*[ SIDER CHECK ]*\n\n*Grup ${conn.getName(m.chat)}, memiliki anggota ${sum} orang dan terdapat sider (penyimak profesional) sebanyak ${total} orang.*\n\n*NB* : *“Akurasi dari fitur ini akan mencapai 85% apabila BOT sudah berada didalam grup minimal 7hr dan fitur ini tidak menghitung admin sider.”*${%readmore}\n\n${sider.map(v => '  ○ @' + v.replace(/@.+/, '')).join('\n')}`, m,{ contextInfo: { mentionedJid: sider } })
 	conn.reply(m.chat, `*[ SIDER CHECK ]*\n\n*Grupo ${conn.getName(m.chat)}, miembros ${sum} participantes (profesional) maximo ${total} personas.*\n\n*NB* : *“Manténgase activo en el grupo porque habrá limpieza de miembros todo el tiempo.”*\n\n*[ LIST SIDER ]*\n${sider.map(v => '  ○ @' + v.replace(/@.+/, '')).join('\n')}`, m,{ contextInfo: { mentionedJid: sider } })
 }
