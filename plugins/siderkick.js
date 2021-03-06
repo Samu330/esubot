c58e7122baf2011e75eafb06dadac15dfe139f46
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-03-06 12:37:59 -0600

    Update siderkick.js

--- plugins/siderkick.js
+++ plugins/siderkick.js
@@ -1,1 +1,56 @@
+let { MessageType, Presence } = require('@adiwajshing/baileys')
+let handler = async (m, { conn, text, participants }) => {
+	await conn.updatePresence(m.chat, Presence.composing) 
+	let member = participants.map(u => u.jid)
+	if(!text) {
+		var sum = member.length
+	} else {
+		var sum = text
+	}
+	var total = 0
 
+	var sider = []
+	for(let i = 0; i < sum; i++) {
+		let users = m.isGroup ? participants.find(u => u.jid == member[i]) : {}
+		if((typeof global.DATABASE.data.users[member[i]] == 'undefined' || global.DATABASE.data.users[member[i]].chat == 0) && !users.isAdmin && !users.isSuperAdmin) { 
+			if (typeof global.DATABASE.data.users[member[i]] !== 'undefined'){
+				if(global.DATABASE.data.users[member[i]].whitelist == false){
+					total++
+					sider.push(member[i])
+				}
+			}else {
+				total++
+				sider.push(member[i])
+			}
+		}
+	}
+
+	if(total == 0) return conn.reply(m.chat, `*Este grupo no tiene fantasmas:D.*`, m) 
+
+	for(let i = 0; i < sum; i++) {
+		let users = m.isGroup ? participants.find(u => u.jid == member[i]) : {}
+		if((typeof global.DATABASE.data.users[member[i]] == 'undefined' || global.DATABASE.data.users[member[i]].chat == 0) && !users.isAdmin && !users.isSuperAdmin) { 
+			if (typeof global.DATABASE.data.users[member[i]] !== 'undefined'){
+				if(global.DATABASE.data.users[member[i]].whitelist == false){
+					await conn.groupRemove(m.chat, [member[i]])
+				}
+			}else {
+				await conn.groupRemove(m.chat, [member[i]])
+			}
+		}
+		global.DATABASE.data.users[member[i]].chat == 0
+	}
+	conn.reply(m.chat, `*Sacado con éxito a ${total} mirones.*`, m)
+}
+handler.help = ['kicksider']
+handler.tags = ['group']
+handler.command = /^(kicksider)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = true
+handler.private = false
+handler.admin = true
+handler.botAdmin = true
+handler.fail = null
+module.exports = handler
