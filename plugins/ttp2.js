fd59e6e5eb486a60adc9c6459889c47dc72ca4f3
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-03-06 09:57:56 -0500

    Update ttp2.js

--- plugins/ttp2.js
+++ plugins/ttp2.js
@@ -1,1 +1,27 @@
 
+const { sticker2 } = require('../lib/sticker')
+const { MessageType } = require('@adiwajshing/baileys')
+
+let handler  = async (m, { conn, text }) => {
+  if (text) {
+    let stiker = await sticker2(null, global.API('xteam', '/ttp', { file: '', text }))
+    conn.sendMessage(m.chat, stiker, MessageType.sticker, {
+      quoted: m
+    })
+  }
+}
+handler.help = ['ttp2 *text*']
+handler.tags = ['sticker']
+handler.command = /^ttp2$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+module.exports = handler
