4454ff3f0751dded8b521ebd3959c2c6f1d1b46b
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-03-07 15:24:44 -0600

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -3,7 +3,7 @@ let handler  = async (m, { conn }) => {
 }
 handler.help = ['']
 handler.tags = []
-handler.command = test
+handler.command = /^(test|es)$/i
 handler.owner = true
 handler.mods = false
 handler.premium = false
