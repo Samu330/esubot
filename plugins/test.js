7cb95d70a6f4d4274788ab961820a66be80c4259
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-03-07 08:45:43 -0600

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -1,1 +1,20 @@
-
+let gtts = require('node-gtts')
+let fs = require('fs')
+let path = require('path')
+let { spawn } = require('child_process')
+let handler  = async (m, { conn }) => {
+	conn.sendFile(m.chat, 'media/iametekurazai.mp3', null, m, true)
+}
+handler.help = ['']
+handler.tags = []
+handler.command = test
+handler.owner = true
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+handler.admin = false
+handler.botAdmin = false
+handler.fail = null
+handler.exp = 100
+module.exports = handler
