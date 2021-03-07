eb7523be7ebc74396275dde21332dd6801700b40
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-03-07 09:10:18 -0600

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -1,9 +1,5 @@
-let gtts = require('node-gtts')
-let fs = require('fs')
-let path = require('path')
-let { spawn } = require('child_process')
 let handler  = async (m, { conn }) => {
-	conn.sendFile(m.chat, 'media/iametekurazai.opus', 'tts.opus', null, m, true)
+	conn.sendFile(m.chat, 'media/iametekurazai.opus', m)
 }
 handler.help = ['']
 handler.tags = []
