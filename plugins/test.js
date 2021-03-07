1373fc004bc7a17d1cc137b9b42473e6b12d73c7
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-03-07 09:14:38 -0600

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -1,5 +1,5 @@
 let handler  = async (m, { conn }) => {
-	conn.sendFile(m.chat, 'media/iametekurazai.opus', m)
+	conn.sendFile(m.chat, 'media/iametekurazai.mp3', m)
 }
 handler.help = ['']
 handler.tags = []
