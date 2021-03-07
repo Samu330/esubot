cb6d9a620c64e6c4bfd1e40fe3f8a8a7b4a1e59f
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-03-07 09:58:39 -0600

    Update everyone.js

--- plugins/everyone.js
+++ plugins/everyone.js
@@ -1,1 +1,17 @@
-
+let handler = async (m, { conn }) => {
+  let ownerGroup = m.chat.split`-`[0] + '@s.whatsapp.net'
+  let users = (await conn.fetchGroupMetadataFromWA(m.chat)).participants.map(u => u.jid)
+  conn.reply(m.chat, `*[ TODOS ]*\n\n*${conn.getName(m.chat)},'  ○ @' + v.replace(/@.+/, '')).join('\n')}`, m})
+  
+handler.help = ['']
+handler.tags = ['']
+handler.command = /^(todos)$/i
+handler.owner = true
+handler.mods = false
+handler.premium = false
+handler.group = true
+handler.private = false
+handler.admin = true
+handler.botAdmin = true
+handler.fail = null
+module.exports = handler
