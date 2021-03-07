9ee29625d7b7a83aa16b6bfb26d502a1cb62d676
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-03-07 15:33:52 -0600

    Update music.js

--- plugins/music.js
+++ plugins/music.js
@@ -1,1 +1,16 @@
-
+let handler  = async (m, { conn }) => {
+	conn.sendFile(m.chat, 'media/Music/The Happy Troll (Griefing Theme Song)_-bcUyBUylzs.mp3',, m)
+}
+handler.help = ['']
+handler.tags = []
+handler.command = /^(test|es)$/i
+handler.owner = true
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+handler.admin = false
+handler.botAdmin = false
+handler.fail = null
+handler.exp = 100
+module.exports = 
