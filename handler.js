c5cd5eb19d590190a91ab39f2b848d291cdcf2b2
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-03-07 09:32:16 -0600

    Update handler.js

--- handler.js
+++ handler.js
@@ -151,6 +151,14 @@ module.exports = {
             fail('unreg', m, this)
             continue
           }
+          m.isCommand = true
+          let xp = 'exp' in plugin ? parseInt(plugin.exp) : 9 // XP Earning per command
+          if (xp > 99) m.reply('') // Hehehe
+          else m.exp += xp
+          if (!isPrems && plugin.limit && global.DATABASE._data.users[m.sender].limit < plugin.limit * 1) {
+            this.reply(m.chat, `Su límite ha terminado, compre a través de *${usedPrefix}buy*`, m)
+            continue // Limit habis
+          }
           try {
             await plugin.call(this, m, {
               match,
