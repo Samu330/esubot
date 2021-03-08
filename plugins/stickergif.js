5648bd6a172bbe90b83b7b96338bbafc867b3bd2
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-03-07 21:18:46 -0500

    Update stickergif.js

--- plugins/stickergif.js
+++ plugins/stickergif.js
@@ -29,10 +29,10 @@ let handler = async (m, { conn, args, usedPrefix }) => {
                 fs.unlinkSync(media)
                 fs.unlinkSync(ran)
             })
-            .addOutputOptions([`-vcodec`, `libwebp`, `-vf`, `scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse`])
+            .addOutputOptions([`-vcodec`, `libwebp`, `-vf`, `scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse`], , { author: 'AUTOR', pack: 'NOMBRE DEL PACK' }))
             .toFormat('webp')
             .save(ran)
-    } else if ((isMedia && m.message.videoMessage.seconds < 11 || isQuotedVideo && m.message.extendedTextMessage.contextInfo.quotedMessage.videoMessage.seconds < 11) && args.length == 0), { author: 'AUTOR', pack: 'NOMBRE DEL PACK' }) {
+    } else if ((isMedia && m.message.videoMessage.seconds < 11 || isQuotedVideo && m.message.extendedTextMessage.contextInfo.quotedMessage.videoMessage.seconds < 11) && args.length == 0, , { author: 'AUTOR', pack: 'NOMBRE DEL PACK' })), {
         const encmedia = isQuotedVideo ? JSON.parse(JSON.stringify(m).replace('quotedM', 'm')).message.extendedTextMessage.contextInfo : m
         const media = await conn.downloadAndSaveMediaMessage(encmedia)
         const ran = getRandom('.webp')
@@ -54,7 +54,7 @@ let handler = async (m, { conn, args, usedPrefix }) => {
                 fs.unlinkSync(media)
                 fs.unlinkSync(ran)
             })
-            .addOutputOptions([`-vcodec`, `libwebp`, `-vf`, `scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse`])
+            .addOutputOptions([`-vcodec`, `libwebp`, `-vf`, `scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse`], , { author: 'AUTOR', pack: 'NOMBRE DEL PACK' }))
             .toFormat('webp')
             .save(ran)
     } else if ((isMedia || isQuotedImage) && args[0] == 'nobg') {
