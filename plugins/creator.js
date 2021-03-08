b617571fd43d206f2354e45d22b0f08bbcd85791
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-03-08 17:11:04 -0600

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -2,8 +2,8 @@ let { Presence } = require('@adiwajshing/baileys')
 let handler  = async (m, { conn }) => {
 	await conn.updatePresence(m.chat, Presence.composing) 
 let name = '👑Samu330👑'
-let number = '529984907794'
-	conn.sendVcard(m.chat, name, number, m)
+let number = 'wa.me//+529984907794'
+	conn.reply(m.chat, name, number, m)
 	conn.sendFile(m.chat, 'media/esu', "*Bot: Esubot*\n\n_Mantenimiento por:_\n\n👑Samu330👑" m)
 }
 
