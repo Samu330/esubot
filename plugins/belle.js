7a49d1b942b3224cb3973d15f7a443d52a5f4722
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-03-08 16:07:03 -0600

    Create belle.js

---
+++ plugins/belle.js
@@ -0,0 +1,36 @@
+let handler  = async (m, { conn }) => {
+  conn.sendFile(m.chat,`“${pickRandom(global.belle)}”`, m)
+}
+handler.help = ['belle']
+handler.tags = ['images']
+handler.command = /^(belle)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+module.exports = handler
+
+function pickRandom(list) {
+  return list[Math.floor(list.length * Math.random())]
+}
+
+//packs by samu330
+global.belle = [
+  "media/belle/belle1.png",
+  "media/belle/belle2.png",
+  "media/belle/belle3.png",
+  "media/belle/belle4.png",
+  "media/belle/belle5.png",
+  "media/belle/belle6.png",
+  "media/belle/belle7.png",
+  "media/belle/belle8.png",
+  "media/belle/belle9.png",
+  "media/belle/belle10.mp4"
+  ]
