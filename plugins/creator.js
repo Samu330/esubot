9d032b5ac074a2209cb37a04908ac0971e137b27
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-03-08 17:37:40 -0600

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,9 @@
 let { Presence } = require('@adiwajshing/baileys')
 let handler  = async (m, { conn }) => {
-	conn.sendFile(m.chat, 'media/esu.jpg', 'wa.me//+529984907794 👑Samu330👑`.', m)
+	m.reply(m.chat, 'media/esu.jpg', 'wa.me//+529984907794 👑Samu330👑', m)
+	m.reply(`wa.me//+529984907794
+👑Samu330👑`.trim())
+	m.reply(`Hola @${m.participant.split`@`[0]}`.trim())
 }
 
 handler.help = ['owner', 'creator']
