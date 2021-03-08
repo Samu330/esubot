1a618133c543f6de7d30d34ee21e572076eb29a2
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-03-08 17:25:46 -0600

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -2,7 +2,7 @@ let { Presence } = require('@adiwajshing/baileys')
 let handler  = async (m, { conn }) => {
 	m.reply(`wa.me//+529984907794
 👑Samu330👑`.trim())
-	conn.sendFile(m.chat, 'media/esu', "*Bot: Esubot*\n\n_Mantenimiento por:_\n\n👑Samu330👑", m)
+	conn.sendFile(m.chat, 'media/esu', '*Bot: Esubot*\n\n_Mantenimiento por:_\n\n👑Samu330👑', m)
 }
 
 handler.help = ['owner', 'creator']
