08cddb790bb5c4251c5bf84df5eab694c1b8df2c
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-03-07 23:40:04 -0500

    Update wame.js

--- plugins/wame.js
+++ plugins/wame.js
@@ -1,4 +1,4 @@
-let handler = async m => m.reply(m.chat, 'Aqui esta su link de whatsapp: wa.me/${number}, m) 
+let handler = async m => m.reply(m.chat, 'Aqui esta su link de whatsapp: wa.me/${number}', m) 
 handler.help = ['wame']
 handler.tags = ['info']
 handler.command = /^wame/i
