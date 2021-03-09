8ef9b7f9495c0d13ed89dbda88083c664bdff58c
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-03-09 08:23:43 -0600

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -5,7 +5,7 @@ handler.before = m => {
   if (m.isBaileys && m.fromMe) return true
   let chat = global.DATABASE.data.chats[m.chat]
   let isGroupLink = linkRegex.exec(m.text)
-  if (chat.antiLink && isGroupLink) m.reply('Eliminando!') conn.groupRemove(m.chat, [user])
+  if (chat.antiLink && isGroupLink) m.reply('Eliminando!') con.groupRemove(m.chat, [user])
   return true
   
 }
