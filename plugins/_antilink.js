ebf289c5abb9e7c38b3aa058addcdd6c166efdb6
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-03-09 08:46:39 -0500

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -3,12 +3,10 @@ let handler = m => m
 let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
 handler.before = m => {
   if (m.isBaileys && m.fromMe) return true
-  let users = m.mentionedJid.filter(u => !(u == ownerGroup || u.includes(conn.user.jid)))
   let chat = global.DATABASE.data.chats[m.chat]
   let isGroupLink = linkRegex.exec(m.text)
 
   if (chat.antiLink && isGroupLink) m.reply('Eliminando!')
-  await conn.groupRemove(m.chat, [user])
   return true
   
 }
