808e2a82e6a35d067fed7350efd18a0f87ac5949
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-03-09 14:50:10 -0600

    Update daily.js

--- plugins/daily.js
+++ plugins/daily.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
   if (new Date - global.DATABASE._data.users[m.sender].lastclaim > 86400000) {
-    global.DATABASE._data.users[m.sender].exp += 9999999999999999999
-    m.reply('+9999999999999999999 te amo samu:D')
+    global.DATABASE._data.users[m.sender].exp += 500
+    m.reply('+500')
     global.DATABASE._data.users[m.sender].lastclaim = new Date * 1
   } else m.reply('Ya has reclamado hoy:)')
 }
