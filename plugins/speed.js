7962a30dfe25c03a52f2f013ccb7cc33c4af7c37
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-03-09 08:16:58 -0600

    Update speed.js

--- plugins/speed.js
+++ plugins/speed.js
@@ -1,8 +1,12 @@
 let { performance } = require('perf_hooks')
 let handler = async (m, { conn }) => {
   let old = performance.now()
+  let hrs = Math.floor(seconds / (60 * 60))
+	let mins = Math.floor(seconds % (60 * 60) / 60)
+  let secs = Math.floor(seconds % 60)
   await m.reply('_Testing speed..._')
   let neww = performance.now()
+  m.reply (' ' + pad(hrs) + ':' + pad(mins) + ':' + pad(secs))
   m.reply(neww - old + 'ms')
 }
 handler.help = ['ping', 'speed']
