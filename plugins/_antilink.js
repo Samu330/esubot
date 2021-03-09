c319c9a204264681d7c556d0c63800543c26c488
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-03-09 08:50:10 -0600

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,13 +1,16 @@
 let handler = m => m
+let handler = async (m, { conn, args }) => {
 
 let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
 handler.before = m => {
   if (m.isBaileys && m.fromMe) return true
   let chat = global.DATABASE.data.chats[m.chat]
   let isGroupLink = linkRegex.exec(m.text)
-  if (chat.antiLink && isGroupLink) m.reply('Eliminando!') con.groupRemove(m.chat, [user])
+  let users = linkRegex.exec(m.text)
+  if (chat.antiLink && isGroupLink) m.reply('Eliminando!') 
+  for (let user of users) if (user.endsWith('@s.whatsapp.net')) await conn.groupRemove(m.chat, [user])
   return true
-  
+ } 
 }
 
 module.exports = handler
