bf13376451e3b658c0cb7bd71e9fd9a4bbc15c0f
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-03-09 22:38:54 -0500

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -1,1 +1,15 @@
+let handler = async m => m.reply(`
+ *GIT DE ESTE BOT*
+https://github.com/Samu330/esubot
+
+MY GIT:
+
+https://github.com/Samu330
+😴                                       
+`.trim()) 
+handler.help = ['git']
+handler.tags = ['info']
+handler.command = /^git$/i
+
+module.exports = handler
 
