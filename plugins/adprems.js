a94ca1a7558379dd1387e71995e44b044963709b
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-03-12 21:53:53 -0500

    😇

---
+++ plugins/adprems.js
@@ -0,0 +1,19 @@
+const { MessageType } = require('@adiwajshing/baileys')
+
+let handler = async(m, { conn, text }) => {
+let who
+  if (m.isGroup) who = m.mentionedJid[0]
+  else who = m.chat
+  if (!who) throw 'Etiquete a alguien, e ingrese el número para verificación !'
+  // if (participants.map(v=>v.jid).includes(global.conn.user.jid)) {
+    global.DATABASE._data.chats[m.chat].premium = true
+  var nomor = m.sender
+    m.reply(`*Listo, usuario agregado✅*\n\n*Numero: wa.me/${nomor.split("@s.whatsapp.net")[0]}\n*Expired:* 30Days\n*Thanks For Added Premium !*`)
+  // } else m.reply('Aquí hay un número de host...')
+}
+handler.help = ['addprems <nomor>']
+handler.tags = ['owner']
+handler.command = /^addprems$/i
+handler.rowner = true
+
+module.exports = handler
