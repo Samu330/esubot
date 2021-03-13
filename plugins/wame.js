e0f1efffbdbbd4bd1713942ba4cfc279cf268a1a
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-03-12 21:34:16 -0500

    Delete wame.js

--- plugins/wame.js
+++
@@ -1,6 +0,0 @@
-let handler = async m => m.reply(m.chat, 'Aqui esta su link de whatsapp: wa.me/${number}', m) 
-handler.help = ['wame']
-handler.tags = ['info']
-handler.command = /^wame/i
-
-module.exports = handler
