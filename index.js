852f950babd92761705f4d42d50fb67f5d98d7df
JotaRoyal <80458356+JotaRoyal@users.noreply.github.com>
2021-03-20 15:35:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let path = require('path')
 let fs = require('fs')
 let package = require('./package.json')
 const CFonts  = require('cfonts')
-CFonts.say('Samu330\nWhatsApp Bot', {
+CFonts.say('JotaRoyal\nWhatsApp Bot', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
