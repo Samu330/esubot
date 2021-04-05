33c7f52ea2891b87885d97d76bf87a4831709dc8
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-04-05 18:51:44 -0500

    ✅

--- plugins/enhance.js
+++ plugins/enhance.js
@@ -5,7 +5,7 @@ let handler = async (m) => {
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
   if (!mime) throw 'Tidak ada foto'
-  if (!/image\/(jpe?g|png)/.test(mime)) throw `Mime ${mime} tidak support`
+  if (!/image\/(jpe?g|png)/.test(mime)) throw `La imagen ${mime} No es soportada`
   let img = await q.download()
   let body = new FormData
   body.append('image', img, 'image')
@@ -14,9 +14,9 @@ let handler = async (m) => {
     body
   })
   if (res.status !== 200) throw await res.json()
-  await conn.sendFile(m.chat, await res.buffer(), 'hd.jpg', 'Nih, hd kan?', m)
+  await conn.sendFile(m.chat, await res.buffer(), 'hd.jpg', 'HD?', m)
 }
-handler.help = ['hd (caption|reply media)', 'enhance (caption|reply media)']
+handler.help = ['hd (caption|reply media)']
 handler.tags = ['tools']
 handler.command = /^(hd|enhance)$/i
 
