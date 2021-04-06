14990b596cfeb965c2ca4645b326690009f3190a
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-04-06 08:38:45 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ let { spawn } = require('child_process')
 let path = require('path')
 let fs = require('fs')
 let package = require('./package.json')
+const CFonts  = require('cfonts')
+CFonts.say('Lightweight\nWhatsApp Bot', {
+  font: 'chrome',
+  align: 'center',
+  gradient: ['red', 'magenta']
+})
+CFonts.say(`'${package.name}' By @${package.author.name || package.author}`, {
+  font: 'console',
+  align: 'center',
+  gradient: ['red', 'magenta']
+})
 
 /**
  * Start a js file
@@ -10,9 +21,11 @@ let package = require('./package.json')
  */
 function start(file) {
   let args = [path.join(__dirname, file), ...process.argv.slice(2)]
-
-
-
+  CFonts.say([process.argv[0], ...args].join(' '), {
+    font: 'console',
+    align: 'center',
+    gradient: ['red', 'magenta']
+  })
   let p = spawn(process.argv[0], args, {
     stdio: ['inherit', 'inherit', 'inherit', 'ipc']
   })
