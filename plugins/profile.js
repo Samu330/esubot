252d70178f79367bc4abc14d2ae87d8c2ef83597
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-04-05 19:01:22 -0500

    ✅

--- plugins/profile.js
+++ plugins/profile.js
@@ -1,4 +1,5 @@
 let PhoneNumber = require('awesome-phonenumber')
+let levelling = require('../lib/levelling')
 let handler = async (m, { conn }) => {
   let pp = './src/avatar_contact.png'
   let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
@@ -7,13 +8,18 @@ let handler = async (m, { conn }) => {
   } catch (e) {
 
   } finally {
-    let name = conn.getName(who)
-    let about = (await conn.getStatus(who)).status
+    let about = (await conn.getStatus(who).catch(console.error) || {}).status || ''
+    let { name, limit, exp, lastclaim, registered, regTime, age, level } = global.DATABASE.data.users[m.sender]
+    let { min, xp, max } = levelling.xpRange(level, global.multiplier)
+    let username = conn.getName(who)
     let str = `
-Name: ${name} (@${who.replace(/@.+/, '')})
-About: ${about}
+Name: ${username} ${registered ? '(' + name + ') ': ''}(@${who.replace(/@.+/, '')})${about ? '\nAbout: ' + about : ''}
 Number: ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
-Link: https://wa.me/${who.split`@`[0]}
+Link: https://wa.me/${who.split`@`[0]}${registered ? '\nAge: ' + age : ''}
+XP: TOTAL ${exp} (${exp - min} / ${xp}) [${max - exp} left to levelup]
+Level: ${level}
+Limit: ${limit}
+Registered: ${registered ? 'Yes (' + new Date(regTime) + ')': 'No'}${lastclaim > 0 ? '\nLast Claim: ' + new Date(lastclaim) : ''}
 `.trim()
     let mentionedJid = [who]
     conn.sendFile(m.chat, pp, 'pp.jpg', str, m, false, { contextInfo: { mentionedJid }})
