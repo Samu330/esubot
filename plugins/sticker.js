e90815ff929965bd7bfaae8b7524adb204da488e
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-04-05 20:01:46 -0500

    Update sticker.js

--- plugins/sticker.js
+++ plugins/sticker.js
@@ -1,33 +1,24 @@
 const { MessageType } = require('@adiwajshing/baileys')
-const { sticker, sticker2 } = require('../lib/sticker')
+const { sticker } = require('../lib/sticker')
 let handler  = async (m, { conn, args }) => {
   let stiker = false
   try {
     let q = m.quoted ? m.quoted : m
-    if (/image/.test(q.mimetype || '')) {
+    let mime = (q.msg || q).mimetype || ''
+    if (/image|video/.test(mime)) {
       let img = await q.download()
-      if (!img) throw img
-      stiker = await sticker2(img)
-    } else if (args[0]) stiker = await sticker2(false, args[0])
+      if (!img) throw 'Envia una Foto o Video'
+      stiker = await sticker(img, false, global.packname, global.author)
+    } else if (args[0]) stiker = await sticker(false, args[0], global.packname, global.author)
   } finally {
     if (stiker) conn.sendMessage(m.chat, stiker, MessageType.sticker, {
       quoted: m
     })
+    else throw 'Conversion failed'
   }
 }
-handler.help = ['stiker (caption|reply media)', 'stiker <url>']
+handler.help = ['stiker (caption|reply media)', 'stiker <url>', 'stikergif (caption|reply media)', 'stikergif <url>']
 handler.tags = ['sticker']
-handler.command = /^stic?ker$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
-
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
+handler.command = /^s(tic?ker)?(gif)?(wm)?$/i
 
 module.exports = handler
-
