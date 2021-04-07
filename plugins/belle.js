e77b656ef6e131af61b95b6df321eb29e0539521
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-04-07 16:48:26 -0500

    Update belle.js

--- plugins/belle.js
+++ plugins/belle.js
@@ -1,13 +1,5 @@
-let fetch = require('node-fetch')
-
-let handler = async(m, { conn, args, command, usedPrefix }) => {
-  fetch('. /belle.jpg').then(res => res.png()).then(body => {
-    let randobelle = body.split('\n')
-    let random = random[Math.floor(Math.random() * randobelle.length)]
-    conn.sendFile(m.chat, random, '', 'error xd pto!! ', m)
-  }).catch(() => {
-    conn.reply(m.chat, ` !`, m)
-  })
+let handler  = async (m, { conn }) => {
+	conn.sendFile(m.chat, './belle.jpg', m)
 }
 
 handler.help = ['belle']
