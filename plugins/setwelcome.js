07cdc0891429e8c342204476f7631db50c944b55
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-04-06 19:31:40 -0500

    Update setwelcome.js

--- plugins/setwelcome.js
+++ plugins/setwelcome.js
@@ -3,12 +3,11 @@ let handler = async (m, { conn, text, isROwner, isOwner }) => {
     if (isROwner) global.conn.welcome = text
     else if (isOwner) conn.welcome = text
     else global.DATABASE._data.chats.sWelcome = text
-    m.reply('La bienvenida se configuró correctamente\n@user (Mention)\n@subject (Título del grupo)')
-  } else throw 'Donde esta el texto?'
+    m.reply('Welcome se a cambiado a:\n@user (Mention)\n@subject (nombre del grupo )')
+  } else throw 'Y el texto ?'
 }
 handler.help = ['setwelcome <teks>']
 handler.tags = ['owner', 'group']
 
 handler.command = /^setwelcome$/i
 module.exports = handler
-
