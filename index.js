73a7eb193ed5fede32334702a97583efc0c96516
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-04-06 19:40:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ let path = require('path')
 let fs = require('fs')
 let package = require('./package.json')
 const CFonts  = require('cfonts')
-CFonts.say('Lightweight\nWhatsApp Bot', {
+CFonts.say('esubot\nWhatsApp Bot', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
 })
-CFonts.say(`'${package.name}' By @${package.author.name || package.author}`, {
+CFonts.say(`'Samu330'`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']
