d2a2bf9dc0e1e8fe6978fbfd49605d83b0f2caf3
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-04-06 19:37:57 -0500

    Update twitter.js

--- plugins/twitter.js
+++ plugins/twitter.js
@@ -1,9 +1,10 @@
 let handler = async (m, { conn, args }) => {
-  if (!args[0]) throw 'Uhm...y el link?'
-  global.API('xteam', '/dl/', {
+  throw // Fitur belum jadi :)
+  if (!args[0]) throw 'Uhm... Y ek link?'
+  global.API('xteam', '/dl/twitter', {
     url: args[0]
   }, 'APIKEY')
-  conn.sendFile(m.chat, '', '', m)
+  conn.sendFile(m.chat, undefined, '', '', m)
 }
 handler.help = [].map(v => v + ' <url>')
 handler.tags = ['downloader']
