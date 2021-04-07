b164441a552a857dc17f25cb679f64ace8b5dbfa
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-04-06 20:01:20 -0500

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = '❣️𝗦𝗮𝗺 𝗬 𝗣𝗲𝗿𝗿𝘆❣️\n    🔥EsuBot🔥'
+global.packname = '❣️𝗦𝗮𝗺 𝗬 𝗣𝗲𝗿𝗿𝘆❣️\n     🔥EsuBot🔥'
 global.author = '🔐𝐒𝐚𝐦𝐮𝟑𝟑𝟎👑'
 
 global.multiplier = 69 // The higher, The harder levelup
