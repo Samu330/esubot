87043402966d0535d91639588e0a376b106574bb
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-04-08 15:20:35 -0500

    Update _Buenos dias.js

--- plugins/_Buenos dias.js
+++ plugins/_Buenos dias.js
@@ -1,6 +1,5 @@
 let handler  = async (m, { conn }) => {
-
        conn.sendFile(m.chat, 'media/Buenos dias.mp3', m)
 }
-handler.command = buenos dias
+handler.command = /^(buenosdias)?(bd)$/i
 module.exports = handler
