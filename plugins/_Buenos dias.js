8cd9a536e6e297c11a6fae8a6e28c3cb8ea7e4bf
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-04-08 15:25:18 -0500

    ✅

--- plugins/_Buenos dias.js
+++ plugins/_Buenos dias.js
@@ -3,7 +3,18 @@ let fs = require('fs')
 let path = require('path')
 let { spawn } = require('child_process')
 let handler  = async (m, { conn }) => {
-       conn.sendFile(m.chat, 'media/Buenos dias.mp3', 'tts.opus', null, m, true)
+       conn.sendFile(m.chat, 'media/Buenos días.mp3', 'tts.opus', null, m, true)
 }
-handler.command = /^(buenosdias)?(bd)$/i
+handler.help = ['']
+handler.tags = []
+handler.command = /^(bd)?(buenos?dias)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+handler.admin = false
+handler.botAdmin = false
+handler.fail = null
+handler.exp = 100
 module.exports = handler
