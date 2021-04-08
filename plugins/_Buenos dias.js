56f4d7be7e40d9c4469a130cccaf9e244ad52af9
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-04-08 15:28:30 -0500

    Update _Buenos dias.js

--- plugins/_Buenos dias.js
+++ plugins/_Buenos dias.js
@@ -5,7 +5,7 @@ let { spawn } = require('child_process')
 let handler  = async (m, { conn }) => {
        conn.sendFile(m.chat, 'media/Buenos días.mp3', 'tts.opus', null, m, true)
 }
-handler.command = /^(bd)?(buenos dias)$/i
+handler.command = /^(bd)$/i
 handler.fail = null
 handler.exp = 100
 module.exports = handler
