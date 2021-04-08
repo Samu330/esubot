a3eafe5c1e6499a68fe4951a32fcebf2793906a6
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-04-08 15:22:29 -0500

    Update _Buenos dias.js

--- plugins/_Buenos dias.js
+++ plugins/_Buenos dias.js
@@ -1,5 +1,9 @@
+let gtts = require('node-gtts')
+let fs = require('fs')
+let path = require('path')
+let { spawn } = require('child_process')
 let handler  = async (m, { conn }) => {
-       conn.sendFile(m.chat, 'media/Buenos dias.mp3', m)
+       conn.sendFile(m.chat, 'media/Buenos dias.mp3', 'tts.opus', null, m, true)
 }
 handler.command = /^(buenosdias)?(bd)$/i
 module.exports = handler
