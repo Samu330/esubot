686a51b04ed198d95f0360518f7a370b2dab9a4d
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-04-08 11:58:00 -0500

    ✨

---
+++ plugins/sombra.js
@@ -0,0 +1,27 @@
+let fetch = require('node-fetch')
+//plugin by Samu330
+
+let handler  = async (m, { conn, text }) => {
+conn.reply(m.chat, '⌛ *EN PROCESO* ⌛', m)
+//let text = args.join` `
+if (!text) return conn.reply(m.chat, 'No hay texto para crear logo', m)
+let url = 'https://videfikri.com/api/textmaker/shadowtext/?text=' + encodeURIComponent(text)
+
+conn.sendFile(m.chat, url, m)
+
+}
+handler.help = ['sombra']
+handler.tags = ['images']
+handler.command = /^sombra?$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+module.exports = handler
