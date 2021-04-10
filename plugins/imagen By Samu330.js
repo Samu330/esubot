233165fcc80ea38b3115a8ba1850e2b505bf0f90
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-04-09 20:07:57 -0500

    Update imagen By Samu330.js

--- plugins/imagen By Samu330.js
+++ plugins/imagen By Samu330.js
@@ -2,13 +2,12 @@ let fetch = require('node-fetch')
 //plugin by Samu330
 
 let handler  = async (m, { conn, text }) => {
-let readMore = more.repeat(4001)
 if (!text) return conn.reply(m.chat, 'No hay texto para buscar imagen', m)
 let url = await fetch('https://api.fdci.se/sosmed/rep.php?gambar=' + encodeURIComponent(text))
 //let url = await fetch('https://api.fdci.se/sosmed/rep.php?gambar=' + encodeURIComponent(text))
 let samu = await url.json()
 let erest = samu[Math.floor(Math.random() * samu.length)]
-            await conn.sendFile(m.chat, erest, '', 'FUERON DEMACIADAS IMAGENES DE *' + encodeURIComponent(text) + '* ESPERO TE GUSTE ESTA!!🥰', ('\n' + readMore + '• _*By @Samu330👑*_ •'), m)
+            await conn.sendFile(m.chat, erest, '', 'FUERON DEMACIADAS IMAGENES DE *' + encodeURIComponent(text) + '* ESPERO TE GUSTE ESTA!!🥰', ? '' : ('\n' + readMore + '• _*By @Samu330👑*_ •'), m)
 
 
 }
