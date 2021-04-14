b761b6c0599cb34732b61367e3eef76a84540e37
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-04-14 17:09:06 -0500

    Update Lesbian By Samu330.js

--- plugins/Lesbian By Samu330.js
+++ plugins/Lesbian By Samu330.js
@@ -1,15 +1,4 @@
 const axios = require('axios')
-let handler = m => m
-let handler = async(m, { conn }) => {
-            let prem = isPrems
-handler.before = function (m, { user }) {
-  if (m.isBaileys && m.fromMe) return true
-  let chat = global.DATABASE.data.chats[m.chat]
-  let isPrems  = prem.exec(m.text)
-
-  if (isPrems) {
-    m.reply('Eliminando!!')
-              
               
 let les = await axios.get('https://meme-api.herokuapp.com/gimme/lesbians')
             conn.sendFile(m.chat, `${les.data.url}`, '', `${les.data.title}`, m)
@@ -19,7 +8,7 @@ handler.tags = ['images']
 handler.command = /^(lesbian)$/i
 handler.owner = false
 handler.mods = false
-handler.premium = false
+handler.premium = true
 handler.group = false
 handler.private = false
 
