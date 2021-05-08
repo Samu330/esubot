d8fd00d3cd22231812bc5a015a34d89e279e9fa8
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-05-08 11:37:24 -0500

    Update imagen By Samu330.js

--- plugins/imagen By Samu330.js
+++ plugins/imagen By Samu330.js
@@ -7,9 +7,7 @@ let url = await fetch('https://api.fdci.se/sosmed/rep.php?gambar=' + encodeURICo
 //let url = await fetch('https://api.fdci.se/sosmed/rep.php?gambar=' + encodeURIComponent(text))
 let samu = await url.json()
 let erest = samu[Math.floor(Math.random() * samu.length)]
-            await conn.sendFile(m.chat, erest, '', 'FUERON DEMACIADAS IMAGENES DE *' + encodeURIComponent(text) + '* ESPERO TE GUSTE ESTA!!🥰', contextInfo: { "forwardingScore": 9999, "isForwarded": true"} , m)
-
-
+            await conn.sendFile(m.chat, erest, '', 'FUERON DEMACIADAS IMAGENES DE *' + encodeURIComponent(text) + '* ESPERO TE GUSTE ESTA!!🥰', m), contextInfo: {forwardingScore": 9999, "isForwarded": true}
 }
 handler.help = ['imagen']
 handler.tags = ['images']
