8bd65b92920d806e5024885a1cb6cfe89dae2b8a
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-05-09 08:29:47 -0500

    Update enable.js

--- plugins/enable.js
+++ plugins/enable.js
@@ -1,14 +1,22 @@
 let handler = async (m, { conn, usedPrefix, command, text, args, isROwner }) => {
   let isEnable = /true|enable|(turn)?on/i.test(command)
   let chat = global.DATABASE._data.chats[m.chat]
+  let user = global.DATABASE._data.users[m.sender]
   let type = (args[0] || '').toLowerCase()
   let isAll = false
+  let isUser = false
   switch (type) {
     case 'welcome':
       chat.welcome = isEnable
       break
+    case 'detect':
+      chat.detect = isEnable
+      break
     case 'delete':
       chat.delete = isEnable
+      break
+    case 'antidelete':
+      chat.delete = !isEnable
       break
     case 'public':
       isAll = true
@@ -21,17 +29,20 @@ let handler = async (m, { conn, usedPrefix, command, text, args, isROwner }) => 
     case 'antilink':
       chat.antiLink = isEnable
       break
+    case 'autolevelup':
+      isUser = true
+      user.autolevelup = isEnable
+      break
     default:
       return m.reply(`
-List option: welcome | delete | public | antilink
-
+List option: welcome | delete | public | antilink | autolevelup | detect
 Ejemplo:
 ${usedPrefix}enable welcome
 ${usedPrefix}disable welcome
 `.trim())
   }
   m.reply(`
-*${type}* se establecio *${isEnable ? 'activado' : 'desactivado'}* en ${isAll ? 'este bot' : 'este chat'}
+*${type}* Se activo*${isEnable ? 'nyala' : 'mati'}* ${isAll ? 'en este bot' : isUser ? '' : 'en este chat'}
 `.trim())
 }
 handler.help = ['en', 'dis'].map(v => v + 'able <option>')
