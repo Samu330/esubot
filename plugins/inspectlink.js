6c9c0eb5d17c594dab101e74ca07df2fe262f308
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-05-26 21:09:34 -0500

    Update inspectlink.js

--- plugins/inspectlink.js
+++ plugins/inspectlink.js
@@ -3,7 +3,7 @@ let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
 let handler = async (m, { conn, text }) => {
   let [_, code] = text.match(linkRegex) || []
   if (!code) throw 'Link invalid'
-  let res = await encodeURIComponent(text)
+  let res = await text.match(linkRegex) || [] 
 
 
 
