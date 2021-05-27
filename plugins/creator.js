a1f2b23e0dde226d2eda31a95490047b6dff73ba
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-05-26 20:50:02 -0500

    🔞

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,6 @@
 let handler = function (m) {
   // this.sendContact(m.chat, '6281515860089', 'Nurutomo', m)
-  this.sendContact(m.chat, '529984907794', '👑Samu330👑', m)
+  this.sendContact(m.chat, '5219984907794', '👑Samu330👑', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
