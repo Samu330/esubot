fcd859708169d3219de0bc77bc9ec9c2509f6b4b
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-06-05 13:50:20 -0500

    Create GetSider.js

---
+++ plugins/GetSider.js
@@ -0,0 +1,39 @@
+let handler = async (m, { conn }) => {
+  if (!m.quoted) throw 'Etiqueta un mensaje!'
+  if (!m.quoted.fromMe) throw false
+  if (!m.quoted.id) throw false
+  let members = m.quoted.chat.endsWith('g.us') ? (await conn.groupMetadata(m.quoted.chat)).participants.length - 1 : m.quoted.chat.endsWith('@broadcast') ? -1 : 1
+  let { reads, deliveries } = await conn.messageInfo(m.quoted.chat, m.quoted.id)
+  let txt = `
+*Leído por:*
+${reads.sort((a, b) => b.t - a.t).map(({ jid, t }) => `@${jid.split`@`[0]}\n_${formatDate(t * 1000)}_`).join('\n')}
+${members > 1 ? `${members - reads.length} restantes` : ''}
+*Entregado a:*
+${deliveries.sort((a, b) => b.t - a.t).map(({ jid, t }) => `wa.me/${jid.split`@`[0]}\n_${formatDate(t * 1000)}_`).join('\n')}
+${members > 1 ? `${members - reads.length - deliveries.length} Faltantes` : ''}
+`.trim()
+  m.reply(txt, null, {
+    contextInfo: {
+      mentionedJid: conn.parseMention(txt)
+    }
+  })
+}
+handler.help = ['getsider']
+handler.tags = ['tools']
+
+handler.command = /^getsider$/
+
+module.exports = handler
+
+function formatDate(n, locale = 'id') {
+  let d = new Date(n)
+  return d.toLocaleDateString(locale, {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric'
+  })
+} 
