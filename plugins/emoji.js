0d96349b85c2c5992b7ef7d89b996d7e1109d9b8
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-06-05 14:02:09 -0500

    Update emoji.js

--- plugins/emoji.js
+++ plugins/emoji.js
@@ -1,26 +1,34 @@
 const { sticker } = require('../lib/sticker')
-const { MessageType } = require('@adiwajshing/baileys')
-
+const fetch = require('node-fetch')
+const defaultType = 'whatsapp'
 let handler = async (m, { usedPrefix, conn, args, text }) => {
   let [tipe, emoji] = text.includes('|') ? text.split('|') : args
   if (tipe && !emoji) {
     emoji = tipe
-    tipe = 'whatsapp'
+    tipe = defaultType
   }
-  if (!emoji) throw `Por favor agregue un emoji\n\nEjemplo ${usedPrefix}emoji whatsapp 😎\n\nList Tipe:
+  if (!emoji) throw `Escribe el tipo de emoji\n\nEjemplo${usedPrefix}emoji whatsapp 😎\n\nList Tipe:
 - whatsapp
 - facebook
 - apple
 - google
 - microsoft`
-  let stiker = await sticker(null, global.API('xteam', '/sticker/emojitopng' + encodeURI(tipe.trim().toLowerCase()), { emo: emoji.trim() }, 'APIKEY'), global.packname, global.author)
-//   m.reply(`
-// Tipe: ${tipe.trim().toLowerCase()}
-// Emoji: ${emoji.trim()}
-// `.trim())
+  emoji = emoji.trim()
+  tipe = tipe.trim().toLowerCase()
+
+  let res = await fetch(global.API('xteam', '/sticker/emojitopng', { emo: emoji.trim() }, 'APIKEY'))
+  if (!res.ok) throw res.text()
+  let json = await res.json()
+  if (!json.status) throw json
+  if (!(tipe in json.result)) tipe = defaultType
+  let stiker = await sticker(null, json.result[tipe].img, global.packname, global.author, [emoji])
+  //   m.reply(`
+  // Tipe: ${tipe}
+  // Emoji: ${emoji}
+  // `.trim())
   m.reply(stiker)
 }
 handler.help = ['emoji [tipe] <emoji>']
 handler.tags = ['sticker']
-handler.command = /^emo(ji)?$/i
+handler.command = /^s?emo(ji)?$/i
 module.exports = handler
