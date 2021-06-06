e58098efd6cfefe1f39d4bd867a989191c4087a4
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-06-05 20:02:26 -0500

    Update and rename plugins/BuscarGrupo.js to complementos/BuscarGrupo.js

--- complementos/BuscarGrupo.js
+++ complementos/BuscarGrupo.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, text }) => {
     if (!text) throw 'Que clase de grupo estas buscando?'
     let res = await fetch(global.API('xteam', '/search/grupwa', {
         q: text
-    }, 'APIKEY'))
+    }, 'd90a9e986e18778b'))
     if (res.status !== 200) throw await res.text()
     let json = await res.json()
     if (!json.status) throw json
